94221366cbd07f032bb58962089205866fdd6166
sirdi <sirdi-volam@gmail.com>
2024-08-11 12:19:52 +0300

    oop, solid

---
+++ 8-solid/app.js
@@ -0,0 +1,40 @@
+'use strict'
+
+class Billing {
+  amount = 2
+}
+
+class FixedBilling extends Billing {
+  calculateTotal() {
+    return this.amount
+  }
+}
+
+class HourBilling extends Billing {
+  constructor(hours) {
+    super()
+    this.hours = hours
+  }
+
+  calculateTotal() {
+    return this.amount * this.hours
+  }
+}
+
+class ItemBilling extends Billing {
+  constructor(items) {
+    super()
+    this.items = items
+  }
+
+  calculateTotal() {
+    return this.amount * this.items
+  }
+}
+
+const fixed = new FixedBilling()
+const hour = new HourBilling(2)
+const item = new ItemBilling(5)
+console.log(fixed.calculateTotal());  // 2
+console.log(hour.calculateTotal());  // 4
+console.log(item.calculateTotal());  // 10
